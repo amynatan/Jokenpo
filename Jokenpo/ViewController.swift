5c6086faf3a89cd9791de795d5a268a77cbc49b3
amynata <amynata.nepomuceno@gmail.com>
2020-04-14 01:02:14 -0400

    Add UI elements to code and add action to buttons (display randomized images on computerView)

--- Jokenpo/ViewController.swift
+++ Jokenpo/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var youView: UIImageView!
+    @IBOutlet weak var computerView: UIImageView!
+    
+    let jogadas = [#imageLiteral(resourceName: "pedra (1)"), #imageLiteral(resourceName: "papel (1)"), #imageLiteral(resourceName: "tesoura (1)")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    
+    @IBAction func rockButtonPressed(_ sender: UIButton) {
+           
+        youView.image = #imageLiteral(resourceName: "pedra (1)")
+        computerView.image = jogadas.randomElement()
+    }
+    
+    
+    @IBAction func paperButtonPressed(_ sender: UIButton) {
+        youView.image = #imageLiteral(resourceName: "papel (1)")
+        computerView.image = jogadas.randomElement()
+    }
+    
+    
+    @IBAction func scissorsButtonPressed(_ sender: UIButton) {
+        youView.image = #imageLiteral(resourceName: "tesoura (1)")
+        computerView.image = jogadas.randomElement()
+    }
+    
 }
 
